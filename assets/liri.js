8b634f7bd298be478db6cf1b22226981c6a70ba1
Ashley <anoelj86@yahoo.com>
2018-06-16 12:20:20 -0600

    added files

---
+++ assets/liri.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+// 10. Make it so liri.js can take in one of the following commands:
+
+//     * `spotify-this-song`
+
+//     * `movie-this`
+
+1. `node liri.js my-tweets`
+
+    * This will show your last 20 tweets and when they were created at in your terminal / bash window.
+
+
